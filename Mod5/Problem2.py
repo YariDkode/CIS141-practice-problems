f2e54160c8ca1ca5d876091aa10a29e74a37531b
Yairelys Perez <tochy16@yahoo.com>
2025-05-11 18:14:49 -0400

    Create Problem2.py

---
+++ Mod5/Problem2.py
@@ -0,0 +1,8 @@
+'''#2. Define a string variable (e.g., my_string = "Olympic College"). Use 
+a for loop to print each character on its own line. Convert each character
+to uppercase before printing.'''
+
+my_string = "Mother's Day"
+
+for char in my_string:
+    print(char.upper())
