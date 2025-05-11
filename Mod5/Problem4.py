9756f028587cb438362af2e75c61e9109baa8cc5
Yairelys Perez <tochy16@yahoo.com>
2025-05-11 18:19:48 -0400

    Create Problem4.py

---
+++ Mod5/Problem4.py
@@ -0,0 +1,14 @@
+'''#4. Use nested for loops to create a simple multiplication table for 
+numbers 1 through 5. Format your output so that each row corresponds 
+to multiplying by a specific number. Example output:
+
+# 1   2   3   4   5
+# 2   4   6   8   10
+# 3   6   9   12  15
+# 4   8   12  16  20
+# 5   10  15  20  25'''
+
+for row in range(1, 6):
+    for col in range(1, 6):
+      print(f"{row * col:<4}", end='') 
+    print()
