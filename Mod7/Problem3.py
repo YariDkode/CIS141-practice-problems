426ce41cf68a00f0ae5f9356a4fec466bebeced3
Yairelys Perez <tochy16@yahoo.com>
2025-05-25 23:55:20 -0400

    Create Problem3.py

---
+++ Mod7/Problem3.py
@@ -0,0 +1,23 @@
+#3. In the game Pokemon, certain types of Pokemon do extra damage to other types.
+For example, water is very effective to fight fire.
+Write a function called type_advantage(attacker, defender) that takes two Pokémon types as
+strings and returns "Super Effective", "Not Very Effective", or "Neutral" based on
+simple type effectiveness rules. Your solution only needs to work for these three sets of input:
+print(type_advantage("Water", "Fire")) # "Super Effective"
+print(type_advantage("Fire", "Water")) # "Not Very Effective"
+print(type_advantage("Electric", "Grass")) # "Neutral"
+'''
+# Type of pokemon effectiveness, if, elif, else, and check
+def type_advantage(attacker, defender):
+    if attacker == "Water" and defender == "Fire":
+        return "Super Effective"
+    elif attacker == "Fire" and defender == "Water":
+        return "Not Very Effective"
+    elif attacker == "Electric" and defender == "Grass":
+        return "Neutral"
+    else:
+        return "Neutral"  
+
+print( type_advantage("Water", "Fire"))      #Super Effective
+print( type_advantage("Fire", "Water"))      #Not Very Effective
+print( type_advantage("Electric", "Grass"))  #Neutral
